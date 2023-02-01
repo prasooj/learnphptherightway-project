ebdba9896325e919f089decfee63d5a489af25cf
Prasooj A S <prasoojas.nma@gmail.com>
2023-02-01 12:37:24 +0530

    obj, class, constaructor

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,15 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
-phpinfo();
+require_once('../public/Transaction.php');
+
+$transaction1 = (new Transaction(100, 'Transaction 1'))
+    ->addTax(8)
+    ->applyDiscount(10);
+
+$transaction2 = (new Transaction(200, 'Transaction 2'))
+    ->addTax(8)
+    ->applyDiscount(15);
+
+var_dump($transaction1->getAmount(), $transaction2->getAmount());
